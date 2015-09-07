e17ccacaa6ba02d682728158a638b402ed172b8f
Mike Rathbun <mikerathbun@me.com>
2015-09-07 02:17:33 +0000

    Added mono ca certificates

--- init/60_dot_net.sh
+++ init/60_dot_net.sh
@@ -4,7 +4,7 @@ e_header "Installing Mono"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
-sudo apt-get install -y mono-complete
+sudo apt-get install -y mono-complete ca-certificates-mono
 
 # docs.asp.net installing on linux
 e_header "Installing DOT.NET Version Manager"
