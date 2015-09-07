2e914c37fd78a26ef006da616257ecfd824a3ee7
Mike Rathbun <mikerathbun@me.com>
2015-09-07 02:20:40 +0000

    Added installation for libuv

--- init/60_dot_net.sh
+++ init/60_dot_net.sh
@@ -28,4 +28,13 @@ sudo apt-get install -r libunwind8 \
 e_header "Installing DNX for Mono"
 dnvm upgrade -r mono
 
-
+e_header "Install libuv"
+sudo apt-get install make automake libtool curl
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
