c6ac05f8223f70c9554259c25c21ac8771be19c8
Mike Rathbun <mikerathbun@me.com>
2017-05-14 21:36:09 -0500

    testing ruby packages

--- init/30_ruby.sh
+++ init/30_ruby.sh
@@ -6,6 +6,16 @@ e_header "Installing rbenv"
 e_header "Adding needed packages"
 
 packages=(
-
+  libyaml-dev
+  libsqlite3-dev
+  sqlite3
+  libxml2-dev
+  libxslt1-dev
+  libcurl4-openssl-dev
+  python-software-properties
+  libffi-dev
+  nodejs
 )
 
+
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
