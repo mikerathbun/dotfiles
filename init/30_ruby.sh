f5b3e92e49c376a91c2ad165dc8c74349faaf9c2
Mike Rathbun <mikerathbun@me.com>
2017-05-15 22:21:58 -0500

    Installing ruby 2.4.1

--- init/30_ruby.sh
+++ init/30_ruby.sh
@@ -32,3 +32,17 @@ fi
 e_header "**** TESTING Installing ruby-build into plugins"
 
 git clone https://github.com/rbenv/ruby-build.git $DOTFILES/vendor/rbenv/plugins/ruby-build
+
+# Install ruby
+if [[ "$(type -P rbenv)" ]]; then
+  versions=(2.4.1)
+
+  rubies=($(setdiff "${versions[*]}" "$(rbenv whence ruby)"))
+  if (( ${#rubies[@]} > 0 )); then
+    e_header "Installing Ruby versions: ${rubies[*]}"
+    for r in "${rubies[@]}"; do
+      rbenv install "$r"
+      [[ "$r" == "${versions[0]}" ]] && rbenv global "$r"
+    done
+  fi
+fi
