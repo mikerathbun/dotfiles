c96d74083b9b7c0445ea75edbd56f0b8fef5bfec
Mike Rathbun <mikerathbun@me.com>
2017-08-19 01:30:05 -0500

    added colors to nvim init script and updated gitignore file to ignore them

--- init/50_nvim.sh
+++ init/50_nvim.sh
@@ -1,5 +1,9 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/nvim
+
+curl -fLo $DOTFILES/link/.config/nvim/colors/molokai.vim --create-dirs \
+  https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+
 curl -fLo $DOTFILES/link/.config/nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
