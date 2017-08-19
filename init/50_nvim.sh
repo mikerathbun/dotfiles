95e115ae385718a49574d4cbc689f64d5f3c59cb
Mike Rathbun <mikerathbun@me.com>
2017-08-19 01:05:54 -0500

    added vim-plug to init file

--- init/50_nvim.sh
+++ init/50_nvim.sh
@@ -1,5 +1,7 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/nvim
+curl -fLo $DOTFILES/link/.config/nvim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Download Vim plugins.
 if [[ "$(type -P nvim)" ]]; then
--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,13 +1,13 @@
 # Editing
 
 if [[ ! "$SSH_TTY" ]] && is_osx; then
-  export EDITOR='mvim'
-  export LESSEDIT='mvim ?lm+%lm -- %f'
+  export EDITOR='nvim'
+  export LESSEDIT='nvim ?lm+%lm -- %f'
 else
   export EDITOR='nvim'
 fi
 
 export VISUAL="$EDITOR"
 alias q="$EDITOR"
-alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
+# alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
 alias qs="q $DOTFILES"
