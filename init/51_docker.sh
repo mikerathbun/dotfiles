2706701a16d4bc2c1c917aea8c5011908a339a18
Mike Rathbun <mikerathbun@me.com>
2018-01-24 20:49:43 +0000

    added -qq to apt-get

--- init/51_docker.sh
+++ init/51_docker.sh
@@ -24,6 +24,6 @@ fi
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https:download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -qq update
+sudo apt-get -qq install docker-ce
 
