e6dbc678d0c17183242a95eefc66e05d730d3a62
Mike Rathbun <mikerathbun@me.com>
2018-02-03 11:47:47 +0000

    removed https from default git protocol and removed sourcing of anaconda virtual wrapper

--- source/31_anaconda.sh
+++ source/31_anaconda.sh
@@ -2,5 +2,5 @@
 export PATH
 
 PATH="$(path_remove $DOTFILES/vendor/anaconda3/bin):$DOTFILES/vendor/anaconda3/bin"
-source /usr/local/bin/virtualenvwrapper.sh
+# source /usr/local/bin/virtualenvwrapper.sh
 
