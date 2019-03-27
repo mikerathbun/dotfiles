d9c5566b4054b844400afc106849891a78a41c75
Mike Rathbun <mikerathbun@me.com>
2019-03-27 15:25:21 -0500

    Updated grep alias
    Updated the alias of grep to pass in the --exclude-dir with
    the .git and backups directories.

--- source/50_file.sh
+++ source/50_file.sh
@@ -36,7 +36,7 @@ alias dsstore="find . -name '*.DS_Store' -type f -ls -delete"
 alias eachdir=". eachdir"
 
 # Make grep have color
-alias grep="egrep --color='auto'"
+alias grep="egrep --color='auto' --exclude-dir={backups,.git}"
 # Create a new directory and enter it
 function md() {
   mkdir -p "$@" && cd "$@"
