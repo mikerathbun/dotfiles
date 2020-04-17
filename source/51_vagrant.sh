35460a5bc1c49f68ee336dac0058e0823a804351
Mike Rathbun <mikerathbun@me.com>
2020-04-17 10:29:18 -0500

    Added Vagrant sources

--- source/51_vagrant.sh
+++ source/51_vagrant.sh
@@ -1,1 +1,10 @@
 VAGRANT_DEFAULT_PROVIDER=virtualbox
+
+alias vga='bat $DOTFILES/source/51_vagrant.sh'
+alias vg='vagrant list-commands | bat'
+alias vgu='vagrant up'
+alias vgv='q Vagrantfile'
+alias vgh='vagrant halt'
+alias vgd='vagrant destroy -f'
+alias vgs='vagrant status'
+alias vgssh='vagrant ssh'
